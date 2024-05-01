ee49653f443ad15a4212e4e3737a689e0467fa74
Axkaps <andre.loja.infante.barbosa@tecnico.ulisboa.pt>
2024-05-01 22:20:45 +0100

    Acabei

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import DropdownMenu from 'v-dropdown-menu'
-import 'v-dropdown-menu/css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(DropdownMenu)
+
 app.mount('#app')
